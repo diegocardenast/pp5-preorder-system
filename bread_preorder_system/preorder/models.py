840c69f5486dabe15927fdeee2e3da7b5613d2d6
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-17 09:33:37 +0000

    feat: create first verison of data models

--- bread_preorder_system/preorder/models.py
+++ bread_preorder_system/preorder/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+"""Data model for the selling points or locations"""
+class sellingPoint(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    googleMapsLink = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+"""Data model for the breads that will be sold"""
+class bread(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='bread_images/')
+    description = models.CharField(max_length=255)
+    canBeSliced = models.BooleanField()
+    canBeInQuarters = models.BooleanField()
+    price = models.IntegerField(max_value=None, min_value=None)
+    pricePerKilogram = models.IntegerField(max_value=None, min_value=None)
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+"""Details that the customer can select when ordering bread"""
+class orderedBread(models.Model):
+    name = models.ForeignKey(bread, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_value=None, min_value=None)
+    sliced = models.BooleanField()
+    quarters = models.IntegerField(max_value=None, min_value=None)
+    totalPrice = models.IntegerField(max_value=None, min_value=None)
+    
+    def __str__(self):
+        return self.name
+
+
+"""Data model for the preorders"""
+class preorder(models.Model):
+    
+    STATUS_CHOICES = [
+        ('received', 'Received'),
+        ('confirmed', 'Confirmed'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sellingPoint = models.ForeignKey(SellingPoint, on_delete=models.CASCADE)
+    preorderPrice = models.IntegerField(max_value=None, min_value=None)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    pickUpDate = models.DateField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} for {self.user.username} on {self.pickUpDate}"
+
+
