e85397de13890d2584c6b03ab0dd21e2d11197f3
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-17 10:32:14 +0000

    feat: add views

--- bread_preorder_system/preorder/views.py
+++ bread_preorder_system/preorder/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import sellingPoint, bread, orderedBread, preorder
+from .serializers import sellingPointSerializer, breadSerializer, orderedBreadSerializer, preorderSerializer
 
 # Create your views here.
+class sellingPointViewSet(viewsets.ModelViewSet):
+    queryset = sellingPoint.objects.all()
+    serializer_class = sellingPointSerializer
+
+class breadViewSet(viewsets.ModelViewSet):
+    queryset = bread.objects.all()
+    serializer_class = breadSerializer
+
+class orderedBreadViewSet(viewsets.ModelViewSet):
+    queryset = orderedBread.objects.all()
+    serializer_class = orderedBreadSerializer
+
+class preorderViewSet(viewsets.ModelViewSet):
+    queryset = preorder.objects.all()
+    serializer_class = preorderSerializer
