8f5a4c13b3a5f1b811a5f06a8940dd53076d5a59
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 15:43:48 +0000

    feat: update backend urls

--- bread_preorder_system/bread_preorder_system/urls.py
+++ bread_preorder_system/bread_preorder_system/urls.py
@@ -19,11 +19,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from bread import urls as bread_urls
+from .views import root_route
 
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('bread.urls')),
     # path('', include('orderedBread.urls')),
     # path('', include('preorder.urls')),
