59c69e354e02b6e40700ce4c78f1c2ef456e173a
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 15:46:22 +0000

    fix: adjust Serializer variables

--- bread_preorder_system/bread_preorder_system/serializers.py
+++ bread_preorder_system/bread_preorder_system/serializers.py
@@ -1,9 +1,11 @@
 from djoser.serializers import UserSerializer, UserCreateSerializer as BaseUserSerializer
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'email', 'username', 'password']
 
-class CurrentUserSerializer(UserSerializer):
-    class Meta(UserSerializer.Meta):
+class CurrentUserSerializer(UserDetailsSerializer):
+    class Meta(UserDetailsSerializer.Meta):
         fields = ['id', 'email', 'username', 'password']
