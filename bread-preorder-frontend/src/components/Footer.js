94bd5c3e80170a108b12834696089a46093ec528
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 16:02:30 +0000

    feat: add navbar and footer

---
+++ bread-preorder-frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className="text-center mt-4">
+            <p>© Copyright Ludwig Stocker Hofpfisterei GmbH, München (DE)</p>
+        </footer>
+    );
+};
+
+export default Footer;
