0fef1a4cdf8f8ca17fd313ab3f2702183753d4c4
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 16:00:50 +0000

    feat: setup home

---
+++ bread-preorder-frontend/src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import indexImage from "../assets/indexImage.jpg"
+import styles from '../styles/Home.module.css';
+
+const Home = ({ isAuthenticated }) => {
+    return (
+        <Container className="text-center mt-4 mb-4">
+            <Row>
+                <Col className={styles.indexCol}>
+                    <img src={indexImage} alt="Women holding bread" className={styles.indexResponsive} />
+                </Col>
+            </Row>
+            <Row>
+                <Col className={styles.colPreorder}>
+                    <p className={styles.text}>Welcome to our bread preorder system. Order your fresh bread online and pick it up at your convenience.</p>
+                    {!isAuthenticated ? (
+                    <Button as={Link} to="/preorder" variant="primary" size="lg" className={styles.btn}>Preorder Now</Button>
+                    ):(<p></p>) }
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Home;
