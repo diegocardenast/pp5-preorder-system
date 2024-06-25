236051c7256b1cfc7ba27a723d6635afcc0646f7
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 16:03:41 +0000

    feat: add routes

--- bread-preorder-frontend/src/App.js
+++ bread-preorder-frontend/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Bread from './components/bread';
-import PreorderForm from './components/preorderForm';
-import SellingPoint from './components/sellingPoint';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+import CustomNavbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PreorderSystem from './pages/PreorderSystem';
+import Account from './pages/Account';
+import YourOrders from './pages/YourOrders';
+import ManageProducts from './pages/ManageProducts';
+import ManageSellingPoints from './pages/ManageSellingPoints';
 
-function App() {
+const App = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
     return (
         <Router>
+            <CustomNavbar isAuthenticated={isAuthenticated} isAdmin={isAdmin} />
             <Routes>
-                <Route path="/" element={<Bread />} />
-                <Route path="/preorder" element={<PreorderForm />} />
-                <Route path="/sellingPoint" element={<SellingPoint />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login setIsAuthenticated={isAuthenticated} />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/preorder" element={<PreorderSystem />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/orders" element={<YourOrders />} />
+                <Route path="/manage-products" element={<ManageProducts />} />
+                <Route path="/manage-selling-points" element={<ManageSellingPoints />} />
             </Routes>
+            <Footer />
         </Router>
     );
-}
+};
 
 export default App;
