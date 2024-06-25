12d1cc3ab841ca564ebdb8dde3e09dc407604e9d
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 20:31:26 +0000

    fix: update views names

--- contactUs/urls.py
+++ contactUs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from contactUs import views
 
 urlpatterns = [
-    path('contactUs/', views.PostList.as_view()),
-    path('contactUs/<int:pk>/', views.PostDetail.as_view())
+    path('contactUs/', views.contactUsViewSet.as_view()),
+    path('contactUs/<int:pk>/', views.contactUsViewSet.as_view())
 ]
 
