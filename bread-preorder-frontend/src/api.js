7aa7f7402b252af69de9017fce8a19d24575cba4
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 16:03:09 +0000

    feat: update URL

--- bread-preorder-frontend/src/api.js
+++ bread-preorder-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'https://8000-diegocarden-pp5preorder-01sol8fjcbf.ws.codeinstitute-ide.net',
     headers: {
         'Content-Type': 'application/json',
     }
