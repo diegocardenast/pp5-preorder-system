49db1ef3e2be43f2cb78582c9d632f2cd0472477
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 20:48:45 +0000

    fix: update url patterns

--- contactUs/urls.py
+++ contactUs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from contactUs import views
 
 urlpatterns = [
-    path('contactUs/', views.contactUsViewSet.as_view()),
-    path('contactUs/<int:pk>/', views.contactUsViewSet.as_view())
+    path('contactUs/', views.ContactUsListApiView.as_view()),
+    path('contactUs/<int:pk>/', views.ContactUsDetailApiView.as_view())
 ]
 
