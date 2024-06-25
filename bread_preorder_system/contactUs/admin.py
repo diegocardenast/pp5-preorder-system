b1a821c7c057deb48d8405372bd3a3e44094c668
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-25 15:42:32 +0000

    feat: setup contact us model

--- bread_preorder_system/contactUs/admin.py
+++ bread_preorder_system/contactUs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from contactUs.models import ContactUs
 
 # Register your models here.
+admin.site.register(ContactUs)
--- bread_preorder_system/contactUs/models.py
+++ bread_preorder_system/contactUs/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-"""Data model for the contact of users"""
-class contactUs(models.Model):
+class ContactUs(models.Model):
+    """Data model for the contact of users"""
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True)
     message = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-createdAt']
 
     def __str__(self):
         return f"Contact request | {self.name}"
