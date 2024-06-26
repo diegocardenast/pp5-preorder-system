d5fbea4ecf818293c35cd1edb61135a8142aaae0
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-26 18:34:07 +0000

    fix: import alert

--- bread-preorder-frontend/src/pages/Register.js
+++ bread-preorder-frontend/src/pages/Register.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Container, Form, Button } from 'react-bootstrap';
+import {
+    Form,
+    Button,
+    Container,
+    Alert,
+  } from "react-bootstrap";
 import styles from '../styles/Register.module.css';
 import axios from 'axios';
 
