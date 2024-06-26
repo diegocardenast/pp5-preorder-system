40aee5cb94746a49d7605b134bb7c88272936c07
Diego Cardenas <diego.cardenast@gmail.com>
2024-06-26 19:03:44 +0000

    test: put debug to True

--- bread_preorder_system/settings.py
+++ bread_preorder_system/settings.py
@@ -64,7 +64,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+# DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-diegocarden-pp5preorder-01sol8fjcbf.ws.codeinstitute-ide.net',
