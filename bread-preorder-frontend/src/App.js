b377e397009e752d22ea291046b86c057afc52ff
Diego Cardenas <diego.cardenast@gmail.com>
2024-07-01 06:03:35 +0000

    feat: update static files

--- bread-preorder-frontend/src/App.js
+++ bread-preorder-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './api/axiosDefaults';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
@@ -13,29 +13,26 @@ import ContactUs from './pages/ContactUs';
 import YourOrders from './pages/YourOrders';
 import ManageProducts from './pages/ManageProducts';
 import ManageSellingPoints from './pages/ManageSellingPoints';
-import { Container } from 'react-bootstrap';
 
 
-function App() {
+const App = () => {
     return (
-        <div>
+        <Router>
             <CustomNavbar />
-            <Container>
-                <Switch>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/preorder" element={<PreorderSystem />} />
-                    <Route path="/account" element={<Account />} />
-                    <Route path="/contact" element={<ContactUs />} />
-                    <Route path="/orders" element={<YourOrders />} />
-                    <Route path="/manage-products" element={<ManageProducts />} />
-                    <Route path="/manage-selling-points" element={<ManageSellingPoints />} />
-                    <Route render={() => <p>Page not found!</p>} />
-                </Switch>
-                <Footer />
-            </Container>
-        </div>
+            <Routes>        
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/preorder" element={<PreorderSystem />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/contact" element={<ContactUs />} />
+                <Route path="/orders" element={<YourOrders />} />
+                <Route path="/manage-products" element={<ManageProducts />} />
+                <Route path="/manage-selling-points" element={<ManageSellingPoints />} />
+                <Route render={() => <p>Page not found!</p>} />
+            </Routes>
+            <Footer />
+        </Router>
     );
 };
 
