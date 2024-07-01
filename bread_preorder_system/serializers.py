84557faf6542ac1cddbe0cc102b0cd8490f285a4
Diego Cardenas <diego.cardenast@gmail.com>
2024-07-01 14:39:04 +0000

    feat: adjust fields shared through API

--- bread_preorder_system/serializers.py
+++ bread_preorder_system/serializers.py
@@ -4,4 +4,5 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields
+        
+        fields = ["last_login", "is_superuser", "username", "first_name", "last_name", "email", "is_staff", "date_joined"]
